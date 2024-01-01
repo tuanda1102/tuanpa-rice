00c06926603cde65c2f25473fab3d4d5d4cf95e7
TuanPA <tuanpa@liftsoft.vn>
2024-01-01 20:35:05 +0700

    fix: env build

--- src/config/appEnv.config.ts
+++ src/config/appEnv.config.ts
@@ -1,11 +1,20 @@
+// const APP_ENV = {
+//   baseUrl: import.meta.env.VERCEL_BASE_URL,
+//   cloudinary: {
+//     name: import.meta.env.VERCEL_CLOUDINARY_NAME,
+//     apiKey: import.meta.env.VERCEL_CLOUDINARY_API_KEY,
+//     apiSecret: import.meta.env.VERCEL_CLOUDINARY_API_SECRET,
+//     apiPreset: import.meta.env.VERCEL_CLOUDINARY_API_UPLOAD_PRESET,
+//   },
+// };
+
 const APP_ENV = {
-  baseUrl: import.meta.env.VERCEL_BASE_URL,
+  baseUrl: 'https://sheet.best/api/sheets/d2fcb7b8-656c-4e66-a9ae-6e3c4a296246',
   cloudinary: {
-    name: import.meta.env.VERCEL_CLOUDINARY_NAME,
-    apiKey: import.meta.env.VERCEL_CLOUDINARY_API_KEY,
-    apiSecret: import.meta.env.VERCEL_CLOUDINARY_API_SECRET,
-    apiPreset: import.meta.env.VERCEL_CLOUDINARY_API_UPLOAD_PRESET,
+    name: 'dkxijt5p0',
+    apiKey: '752713386867686',
+    apiSecret: 'lCNWvsDGjbH9fCqkawl6nM6OPy8',
+    apiPreset: 'zjaeu0om',
   },
 };
-
 export default APP_ENV;
