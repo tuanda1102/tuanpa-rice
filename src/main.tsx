8067ce0f91a110817a1c74ca4eb0400c41a0eb76
trieu nguyen <trieunm@liftsoft.vn>
2024-01-16 10:34:14 +0700

    fix(Hook): import library css apexchart in globalStyleProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { GoogleOAuthProvider } from '@react-oauth/google';
-import 'apexcharts/dist/apexcharts.css';
 
 import App from './App.tsx';
 import './index.css';
--- src/providers/GlobalStylesProvider.tsx
+++ src/providers/GlobalStylesProvider.tsx
@@ -1,5 +1,6 @@
 import { NextUIProvider, type NextUIProviderProps } from '@nextui-org/react';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import 'apexcharts/dist/apexcharts.css';
 
 function GlobalStylesProvider({ children, ...passProps }: NextUIProviderProps) {
   return (
