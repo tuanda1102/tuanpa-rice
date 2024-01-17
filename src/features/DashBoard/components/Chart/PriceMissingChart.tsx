3144a960b10641892819c8dbfaeffd086fa9da17
sinhlv <sinhlv@liftsoft.vn>
2024-01-17 16:45:24 +0700

    feature: show chart dashboard

--- src/features/DashBoard/components/Chart/PriceMissingChart.tsx
+++ src/features/DashBoard/components/Chart/PriceMissingChart.tsx
@@ -39,12 +39,27 @@ export default function PriceMissingChart({
           label='Loading...'
         />
       ) : (
-        <ReactApexChart
-          options={options}
-          series={[precentMissing]}
-          type='radialBar'
-          height={350}
-        />
+        <>
+          <h2 className='text-center font-[500] text-2xl m-0 '>
+            Số tiền còn thiếu để sinh tồn!!!
+          </h2>
+          <ReactApexChart
+            options={options}
+            series={[precentMissing]}
+            type='radialBar'
+            height={350}
+          />
+          <div className='flex items-center justify-center gap-x-5'>
+            <div className='flex items-center gap-x-3'>
+              <span className='w-5 rounded-full h-5 bg-[#008FFB]' />
+              <span>Tiền đã thanh toán</span>
+            </div>
+            <div className='flex items-center gap-x-3'>
+              <span className='w-5 rounded-full h-5 border  bg-[#F2F2F2]' />
+              <span>Tiền chưa thanh toán</span>
+            </div>
+          </div>
+        </>
       )}
     </div>
   );
