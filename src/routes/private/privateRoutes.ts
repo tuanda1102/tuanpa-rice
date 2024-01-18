15097f74370abfcc223f5ed0fac0ca702199f9ec
trieu nguyen <trieunm@liftsoft.vn>
2024-01-18 11:07:36 +0700

    fix(routes): fix import rankDonateRoutes

--- src/routes/private/privateRoutes.ts
+++ src/routes/private/privateRoutes.ts
@@ -1,6 +1,6 @@
 import { type IRoute } from '@/types/routes';
 import newFeedsRoutes from '@/features/NewFeeds/routes/newFeeds.routes';
-import rankDonateRoutes from '@/features/RankDonates/routes/RankDonate.routes';
+import rankDonateRoutes from '@/features/RankDonates/routes/rankDonate.routes';
 
 const privateRoutes: IRoute[] = [...newFeedsRoutes, ...rankDonateRoutes];
 
