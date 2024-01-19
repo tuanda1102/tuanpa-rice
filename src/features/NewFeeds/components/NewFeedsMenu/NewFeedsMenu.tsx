e400068c35d388b4176551e6a43c4be27fcfacc5
trieu nguyen <trieunm@liftsoft.vn>
2024-01-19 15:08:52 +0700

    feature(DatePicker): create datapicker and filter array menuList

--- src/features/NewFeeds/components/NewFeedsMenu/NewFeedsMenu.tsx
+++ src/features/NewFeeds/components/NewFeedsMenu/NewFeedsMenu.tsx
@@ -1,5 +1,9 @@
 import { ScrollShadow } from '@nextui-org/react';
 import { CgCoffee } from 'react-icons/cg';
+import { useMemo, useState } from 'react';
+import { BsCalendarDate } from 'react-icons/bs';
+import DatePicker from 'react-datepicker';
+import { Timestamp } from 'firebase/firestore';
 
 import { useMenus } from '@/apis/order.api';
 import MenuItem from '@/features/NewFeeds/components/MenuItem/MenuItem';
@@ -7,25 +11,59 @@ import NewFeedsMenuHeader from '@/features/NewFeeds/components/NewFeedsMenu/NewF
 
 function NewFeedsMenu() {
   const { menuList, isSuccess } = useMenus();
+  const today = new Date();
+  const [datePickerValue, setDatePickerValue] = useState<Date | null>(today);
+
+  const matchedDatelist = useMemo(() => {
+    return menuList?.filter((item) => {
+      if (item.createdAt instanceof Timestamp) {
+        return (
+          item.createdAt.toDate().toDateString() ===
+          datePickerValue?.toDateString()
+        );
+      }
+      return false; // Handle the case where createdAt is not a Timestamp
+    });
+  }, [menuList, datePickerValue]);
+
+  const handleSubmitDate = (date: Date | null) => {
+    setDatePickerValue(date);
+  };
 
   return (
-    <div className='min-w-[480px] pt-4 flex flex-col'>
-      <NewFeedsMenuHeader />
-      <ScrollShadow className='w-full flex-grow px-2'>
-        {menuList?.length
-          ? menuList.map((menu) => <MenuItem key={menu.id} menu={menu} />)
-          : ''}
-
-        {isSuccess && !menuList?.length ? (
-          <div className='flex flex-col items-center gap2 text-foreground-400'>
-            <CgCoffee size={48} />
-            <h5>Không có menu để hiển thị</h5>
+    <>
+      <div className='min-w-[480px] pt-4 flex flex-col'>
+        <div className='flex flex-row justify-between'>
+          <div className='flex flex-row items-center h-[32px] hover:border-blue-500 hover:rounded-lg hover:border-3 justify-between'>
+            <BsCalendarDate className='text-black dark:text-white' size={24} />
+            <DatePicker
+              maxDate={datePickerValue}
+              placeholderText='Select date'
+              onChange={(date) => handleSubmitDate(date)}
+              selected={datePickerValue}
+              className='border-none focus:outline-none pl-2 rounded-lg w-[110px]'
+            />
           </div>
-        ) : (
-          ''
-        )}
-      </ScrollShadow>
-    </div>
+          <NewFeedsMenuHeader />
+        </div>
+        <ScrollShadow className='w-full flex-grow px-2'>
+          {menuList && matchedDatelist?.length
+            ? matchedDatelist.map((menu) => (
+                <MenuItem key={menu.id} menu={menu} />
+              ))
+            : ''}
+
+          {isSuccess && !matchedDatelist?.length ? (
+            <div className='flex flex-col items-center gap2 text-foreground-400'>
+              <CgCoffee size={48} />
+              <h5>Không có menu để hiển thị</h5>
+            </div>
+          ) : (
+            ''
+          )}
+        </ScrollShadow>
+      </div>
+    </>
   );
 }
 
