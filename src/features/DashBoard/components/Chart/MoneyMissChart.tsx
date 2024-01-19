90279ec5f1f96034b2ddf1ad0a5053ff1a850127
sinhlv <sinhlv@liftsoft.vn>
2024-01-19 17:44:41 +0700

    refactor: refactor dashboard

--- src/features/DashBoard/components/Chart/MoneyMissChart.tsx
+++ src/features/DashBoard/components/Chart/MoneyMissChart.tsx
@@ -1,23 +1,23 @@
 import { useId, useMemo } from 'react';
 import ReactApexChart from 'react-apexcharts';
-import { Spinner } from '@nextui-org/react';
+import { Card, CardBody, Spinner } from '@nextui-org/react';
 import { type IOrder } from '@/types/order';
 import { formatterPrice } from '@/utils/prices.util';
 import { totalPrice } from '@/features/DashBoard/utils/dashBoard.util';
 import { priceMissingChartOptions } from './ChartOptions/moneyMissingOptions';
+import { useGetAllOrders } from '@/apis/order.api';
 
-interface IPriceMissingProps {
-  allOrders: IOrder[];
-  allOrderMissing: IOrder[];
-  isLoading: boolean;
-}
+export default function PriceMissingChart() {
+  const { allOrders, isLoading } = useGetAllOrders();
 
-export default function PriceMissingChart({
-  allOrders,
-  allOrderMissing,
-  isLoading,
-}: IPriceMissingProps) {
-  const totalPriceMissing = -totalPrice(allOrderMissing);
+  const allOrderMissing = allOrders.filter(
+    (item: IOrder) => item.status === false,
+  );
+
+  const totalPriceMissing = useMemo(
+    () => -totalPrice(allOrderMissing),
+    [allOrderMissing],
+  );
 
   const precentPaid = useMemo(
     () =>
@@ -40,27 +40,29 @@ export default function PriceMissingChart({
           label='Loading...'
         />
       ) : (
-        <>
-          <h2 className='text-center font-[500] text-2xl m-0 '>
-            Số tiền còn thiếu để sinh tồn!!!
-          </h2>
-          <ReactApexChart
-            options={options}
-            series={[precentPaid]}
-            type='radialBar'
-            height={350}
-          />
-          <div className='flex items-center justify-center gap-x-5'>
-            <div className='flex items-center gap-x-3'>
-              <span className='w-5 rounded-full h-5 bg-[#008FFB]' />
-              <span>Tiền đã thanh toán</span>
+        <Card>
+          <CardBody>
+            <h2 className='text-center font-[500] text-2xl m-0 '>
+              Số tiền còn thiếu để sinh tồn!!!
+            </h2>
+            <ReactApexChart
+              options={options}
+              series={[precentPaid]}
+              type='radialBar'
+              height={350}
+            />
+            <div className='flex items-center justify-center gap-x-5'>
+              <div className='flex items-center gap-x-3'>
+                <span className='w-5 rounded-full h-5 bg-[#008FFB]' />
+                <span>Tiền đã thanh toán</span>
+              </div>
+              <div className='flex items-center gap-x-3'>
+                <span className='w-5 rounded-full h-5 border  bg-[#F2F2F2]' />
+                <span>Tiền chưa thanh toán</span>
+              </div>
             </div>
-            <div className='flex items-center gap-x-3'>
-              <span className='w-5 rounded-full h-5 border  bg-[#F2F2F2]' />
-              <span>Tiền chưa thanh toán</span>
-            </div>
-          </div>
-        </>
+          </CardBody>
+        </Card>
       )}
     </div>
   );
--- src/features/DashBoard/components/Table/MoneyMissingTable.tsx
+++ src/features/DashBoard/components/Table/MoneyMissingTable.tsx
@@ -9,86 +9,100 @@ import {
   Spinner,
   Button,
   useDisclosure,
+  Card,
+  CardBody,
 } from '@nextui-org/react';
+import { useMemo } from 'react';
 import { type IOrder } from '@/types/order';
 import { formatterPrice } from '@/utils/prices.util';
 import ConfirmPaidModal from '@/features/DashBoard/components/Modal/ConfirmPaidModal';
 import { calculateRankMissing } from '@/features/DashBoard/utils/dashBoard.util';
 import { useFetchUser } from '@/apis/user.api';
+import { useGetAllOrders } from '@/apis/order.api';
 
-interface ITableProps extends TableProps {
-  allOrderMissing: IOrder[];
-  isLoading: boolean;
-}
-function MoneyMissingTable({
-  allOrderMissing,
-  isLoading,
-  ...passProps
-}: ITableProps) {
+function MoneyMissingTable({ ...passProps }: TableProps) {
   const { onClose, onOpen, onOpenChange, isOpen } = useDisclosure();
+
+  const { allOrders, isLoading } = useGetAllOrders();
+
+  const allOrderMissing = allOrders.filter(
+    (item: IOrder) => item.status === false,
+  );
 
   const { authUser } = useFetchUser();
 
-  const rankUserMissing = calculateRankMissing(allOrderMissing).sort(
-    (a, b) => b.totalAmount - a.totalAmount,
+  const rankUserMissing = useMemo(
+    () =>
+      calculateRankMissing(allOrderMissing).sort(
+        (a, b) => b.totalAmount - a.totalAmount,
+      ),
+    [allOrderMissing],
   );
 
   return (
-    <Table
-      isHeaderSticky
-      shadow='none'
-      selectionMode='single'
-      aria-label='table-order'
-      defaultSelectedKeys={[authUser?.email as string]}
-      classNames={{
-        base: 'max-h-[400px] overflow-scroll',
-        table: 'min-h-[450px]',
-      }}
-      {...passProps}
-    >
-      <TableHeader>
-        <TableColumn width={80}>STT</TableColumn>
-        <TableColumn width={230}>Tên người dùng</TableColumn>
-        <TableColumn width={230}>Tổng nợ</TableColumn>
-        <TableColumn width={230}>Thanh toán</TableColumn>
-      </TableHeader>
+    <Card>
+      <CardBody>
+        <Table
+          isHeaderSticky
+          shadow='none'
+          selectionMode='single'
+          aria-label='table-order'
+          defaultSelectedKeys={[authUser?.email as string]}
+          classNames={{
+            base: 'max-h-[400px] overflow-scroll',
+            table: 'min-h-[450px]',
+          }}
+          {...passProps}
+        >
+          <TableHeader>
+            <TableColumn width={80}>STT</TableColumn>
+            <TableColumn width={230}>Tên người dùng</TableColumn>
+            <TableColumn width={230}>Tổng nợ</TableColumn>
+            <TableColumn width={230}>Thanh toán</TableColumn>
+          </TableHeader>
 
-      <TableBody
-        loadingContent={<Spinner label='Loading...' />}
-        isLoading={isLoading}
-        items={rankUserMissing || []}
-      >
-        {rankUserMissing?.length
-          ? rankUserMissing.map((ordered, index) => (
-              <TableRow key={ordered.userEmail}>
-                <TableCell>{index + 1}</TableCell>
+          <TableBody
+            loadingContent={<Spinner label='Loading...' />}
+            isLoading={isLoading}
+            items={rankUserMissing || []}
+          >
+            {rankUserMissing?.length
+              ? rankUserMissing.map((ordered, index) => (
+                  <TableRow key={ordered.userEmail}>
+                    <TableCell>{index + 1}</TableCell>
 
-                <TableCell>{ordered.userEmail}</TableCell>
+                    <TableCell>{ordered.userEmail}</TableCell>
 
-                <TableCell>
-                  {formatterPrice.format(ordered.totalAmount)}
-                </TableCell>
-                <TableCell>
-                  {authUser?.email === ordered.userEmail && (
-                    <>
-                      <Button color='success' variant='ghost' onPress={onOpen}>
-                        Thanh toán
-                      </Button>
-                      <ConfirmPaidModal
-                        userEmail={authUser.email}
-                        totalMissing={ordered.totalAmount}
-                        isOpen={isOpen}
-                        onOpenChange={onOpenChange}
-                        onClose={onClose}
-                      />
-                    </>
-                  )}
-                </TableCell>
-              </TableRow>
-            ))
-          : []}
-      </TableBody>
-    </Table>
+                    <TableCell>
+                      {formatterPrice.format(ordered.totalAmount)}
+                    </TableCell>
+                    <TableCell>
+                      {authUser?.email === ordered.userEmail && (
+                        <>
+                          <Button
+                            color='success'
+                            variant='ghost'
+                            onPress={onOpen}
+                          >
+                            Thanh toán
+                          </Button>
+                          <ConfirmPaidModal
+                            userEmail={authUser.email}
+                            totalMissing={ordered.totalAmount}
+                            isOpen={isOpen}
+                            onOpenChange={onOpenChange}
+                            onClose={onClose}
+                          />
+                        </>
+                      )}
+                    </TableCell>
+                  </TableRow>
+                ))
+              : []}
+          </TableBody>
+        </Table>
+      </CardBody>
+    </Card>
   );
 }
 
