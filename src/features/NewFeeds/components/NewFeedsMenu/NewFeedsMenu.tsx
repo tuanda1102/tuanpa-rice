dd2c28d4d48c9e8d30ceb66da4da13f5a66c7740
trieu nguyen <trieunm@liftsoft.vn>
2024-01-21 17:53:48 +0700

    feature(NewFeedsMenu): datepicker app

--- src/features/NewFeeds/components/NewFeedsMenu/NewFeedsMenu.tsx
+++ src/features/NewFeeds/components/NewFeedsMenu/NewFeedsMenu.tsx
@@ -1,59 +1,55 @@
 import { ScrollShadow } from '@nextui-org/react';
 import { CgCoffee } from 'react-icons/cg';
-import { useMemo, useState } from 'react';
-import { BsCalendarDate } from 'react-icons/bs';
-import DatePicker from 'react-datepicker';
-import { Timestamp } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+import { useForm, FormProvider } from 'react-hook-form';
+import { startOfDay, endOfDay } from 'date-fns';
 
 import { useMenus } from '@/apis/order.api';
 import MenuItem from '@/features/NewFeeds/components/MenuItem/MenuItem';
 import NewFeedsMenuHeader from '@/features/NewFeeds/components/NewFeedsMenu/NewFeedsMenuHeader';
+import CDatePicker from '@/components/DatePicker/CDatePicker';
 
 function NewFeedsMenu() {
-  const { menuList, isSuccess } = useMenus();
-  const today = new Date();
-  const [datePickerValue, setDatePickerValue] = useState<Date | null>(today);
-
-  const matchedDatelist = useMemo(() => {
-    return menuList?.filter((item) => {
-      if (item.createdAt instanceof Timestamp) {
-        return (
-          item.createdAt.toDate().toDateString() ===
-          datePickerValue?.toDateString()
-        );
-      }
-      return false; // Handle the case where createdAt is not a Timestamp
-    });
-  }, [menuList, datePickerValue]);
-
-  const handleSubmitDate = (date: Date | null) => {
-    setDatePickerValue(date);
+  const [dateValue, setDateValue] = useState<Date>(new Date());
+  const startAt = (date: Date): Date => {
+    const parsed = startOfDay(date);
+    return parsed;
+  };
+  const endAt = (date: Date): Date => {
+    const parsed = endOfDay(date);
+    return parsed;
   };
 
+  const startDate = startAt(dateValue);
+  const endDate = endAt(dateValue);
+
+  const { menuList, isLoading, isSuccess } = useMenus(startDate, endDate);
+
+  const methods = useForm();
+  const { watch } = methods;
+  const dateWatch = watch('date');
+
+  useEffect(() => {
+    setDateValue(dateWatch);
+  }, [dateWatch]);
   return (
     <>
       <div className='min-w-[480px] pt-4 flex flex-col'>
         <div className='flex flex-row justify-between'>
-          <div className='flex flex-row items-center h-[32px] hover:border-blue-500 hover:rounded-lg hover:border-3 justify-between'>
-            <BsCalendarDate className='text-black dark:text-white' size={24} />
-            <DatePicker
-              maxDate={datePickerValue}
-              placeholderText='Select date'
-              onChange={(date) => handleSubmitDate(date)}
-              selected={datePickerValue}
-              className='border-none focus:outline-none pl-2 rounded-lg w-[110px]'
-            />
-          </div>
+          <FormProvider {...methods}>
+            <form>
+              <CDatePicker name='date' id='date' isLoading={isLoading} />
+            </form>
+          </FormProvider>
           <NewFeedsMenuHeader />
         </div>
-        <ScrollShadow className='w-full flex-grow px-2'>
-          {menuList && matchedDatelist?.length
-            ? matchedDatelist.map((menu) => (
-                <MenuItem key={menu.id} menu={menu} />
-              ))
+
+        <ScrollShadow className='w-full flex-grow p-2'>
+          {menuList && menuList?.length
+            ? menuList.map((menu) => <MenuItem key={menu.id} menu={menu} />)
             : ''}
 
-          {isSuccess && !matchedDatelist?.length ? (
+          {isSuccess && !menuList?.length ? (
             <div className='flex flex-col items-center gap2 text-foreground-400'>
               <CgCoffee size={48} />
               <h5>Không có menu để hiển thị</h5>
--- src/providers/GlobalStylesProvider.tsx
+++ src/providers/GlobalStylesProvider.tsx
@@ -1,7 +1,6 @@
 import { NextUIProvider, type NextUIProviderProps } from '@nextui-org/react';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import 'apexcharts/dist/apexcharts.css';
-import 'react-datepicker/dist/react-datepicker.css';
 
 function GlobalStylesProvider({ children, ...passProps }: NextUIProviderProps) {
   return (
