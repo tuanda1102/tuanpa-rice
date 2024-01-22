c3a84d3ab5acba2bec9866fbbe0f12dcafe79bc0
trieu nguyen <trieunm@liftsoft.vn>
2024-01-22 08:32:55 +0700

    fix(SideBarApp): fix  data in listSidebar

--- src/components/Layouts/Sidebar.tsx
+++ src/components/Layouts/Sidebar.tsx
@@ -1,37 +1,7 @@
 import { Image } from '@nextui-org/react';
 import { Link } from 'react-router-dom';
-import { AiOutlineHome } from 'react-icons/ai';
-import { FaRankingStar } from 'react-icons/fa6';
-import { MdOutlineRestaurantMenu } from 'react-icons/md';
 
-import { PRIVATE_URL } from '@/constants/routerUrl';
 import ListSidebar from '@/components/SidebarItem/ListSidebar';
-
-const dataListSide = [
-  {
-    id: 1,
-    name: 'Menu',
-    link: PRIVATE_URL.newFeeds,
-    icon: (
-      <MdOutlineRestaurantMenu
-        className='text-black  dark:text-white '
-        size={24}
-      />
-    ),
-  },
-  {
-    id: 2,
-    name: 'Dashboard',
-    link: PRIVATE_URL.dashboard,
-    icon: <AiOutlineHome className='text-black  dark:text-white ' size={24} />,
-  },
-  {
-    id: 3,
-    name: 'RankDonates',
-    link: PRIVATE_URL.rankDonate,
-    icon: <FaRankingStar className='text-black dark:text-white ' size={24} />,
-  },
-];
 
 function Sidebar() {
   return (
@@ -42,7 +12,6 @@ function Sidebar() {
     >
       <div className='h-full w-[200px] border-r-2 px-3 py-4 flex flex-col justify-between overflow-y-auto'>
         <div>
-          {' '}
           <Link to='/'>
             <Image
               className='mb-10'
@@ -51,7 +20,7 @@ function Sidebar() {
               src='https://scontent.fdad1-4.fna.fbcdn.net/v/t1.6435-9/129351049_2943481109206930_5974791713550558502_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=dd63ad&_nc_ohc=M8VZemg_yFMAX9uoZ8e&_nc_ht=scontent.fdad1-4.fna&oh=00_AfDCb849-N7znv7AQHu1QwGCc5eAKkx9EITZrtHo7CbN7w&oe=65B5FF54'
             />
           </Link>
-          <ListSidebar data={dataListSide} />
+          <ListSidebar />
         </div>
         <div>
           <span className='text-secondary'>v2.0.0</span>
--- src/components/SidebarItem/ListSidebar.tsx
+++ src/components/SidebarItem/ListSidebar.tsx
@@ -1,5 +1,10 @@
 import { type ReactElement } from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import { AiOutlineHome } from 'react-icons/ai';
+import { FaRankingStar } from 'react-icons/fa6';
+import { MdOutlineRestaurantMenu } from 'react-icons/md';
+
+import { PRIVATE_URL } from '@/constants/routerUrl';
 
 interface IDataListSide {
   id: number;
@@ -8,17 +13,42 @@ interface IDataListSide {
   icon: ReactElement;
 }
 
-function ListSidebar({ data }: { data: IDataListSide[] }) {
+const dataListSide = [
+  {
+    id: 1,
+    name: 'Menu',
+    link: PRIVATE_URL.newFeeds,
+    icon: (
+      <MdOutlineRestaurantMenu
+        className='text-black  dark:text-white '
+        size={24}
+      />
+    ),
+  },
+  {
+    id: 2,
+    name: 'Dashboard',
+    link: PRIVATE_URL.dashboard,
+    icon: <AiOutlineHome className='text-black  dark:text-white ' size={24} />,
+  },
+  {
+    id: 3,
+    name: 'RankDonates',
+    link: PRIVATE_URL.rankDonate,
+    icon: <FaRankingStar className='text-black dark:text-white ' size={24} />,
+  },
+];
+
+function ListSidebar() {
   const { pathname } = useLocation();
   return (
     <>
-      {data?.map((item: IDataListSide) => (
+      {dataListSide?.map((item: IDataListSide) => (
         <div
           key={item.id}
           className='w-full py-2 hover:bg-[#F5F7F8] hover:rounded-lg dark:hover:bg-blue-600'
         >
           <Link to={item?.link} className='p-2 flex flex-row justify-start'>
-            {' '}
             <div>{item?.icon}</div>
             <p
               className={`px-3 text-lg ${
